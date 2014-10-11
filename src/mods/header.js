a2be4575956ad95e26d167876f3d3c0ce9e828ef
yongqiang.sun <yongqiang.sun@jimubox.com>
2014-10-11 12:53:34 +0800

    dd

--- src/mods/header.js
+++
@@ -1,7 +0,0 @@
-var $ = require('node').all;
-module.exports = {
-    init:function(){
-        S.log('header init');
-        $('header').html('this is header');
-    }
-}
--- test/spec/index-spec.js
+++
@@ -1,9 +0,0 @@
-KISSY.add(function (S,require) {
-    var index = require('bee/index');
-    describe('bee index', function () {
-        it('index init',function(){
-            expect(index).toBeTruthy();
-        })
-    });
-
-});
